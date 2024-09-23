fea55a8bd556bbdf8a458bf02bdb7481593fc846
Mugesh <mugeshmac@gmail.com>
2024-09-23 14:04:31 +0530

    head-foot

--- src/Head-Foot/Header.js
+++ src/Head-Foot/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './CSS/Header.css';
-import logo from '../icons/logo.png';
+import logo from '../icons/logo1.png';
 
 const Header = () => {
   return (
     <header className="header">
-      <img src={logo} alt="Logo" />
+      <a href='http://localhost:3000/'>
+       <img src={logo} alt="Logo" />
+      </a>
       <nav>
         <ul className="nav-links">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><a href="http://localhost:3000/">Home</a></li>
+          <li><a href="http://localhost:3000/">MyProfile</a></li>
+          <li><a href="http://localhost:3000/">Logout</a></li>
         </ul>
       </nav>
     </header>
