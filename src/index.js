c37b0c1b2c0ee2008c3fa8f1ca3f8c7efc0344b1
Mugesh <mugeshmac@gmail.com>
2024-09-27 20:51:32 +0530

    added myinfo + merged mysql + recorded session

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import TestLogin from './test_login/test_login';
-import PrivateRoute from './PrivateRoute'; // Import PrivateRoute
+import PrivateRoute from './PrivateRoute'; 
 import reportWebVitals from './reportWebVitals';
+import MyInfo from './myinfo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +15,7 @@ root.render(
       <Routes>
         <Route path="/" element={<TestLogin />} />
         <Route path="/app" element={<PrivateRoute><App /></PrivateRoute>} />
+        <Route path="/myinfo" element={<MyInfo />} />
       </Routes>
     </Router>
   </React.StrictMode>
